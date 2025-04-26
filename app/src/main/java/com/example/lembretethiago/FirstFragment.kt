c2e049b45cf5f7398bb8911b58949be1baa75bd8
carlosbrenoe <4500445810@estudante.sed.sc.gov.br>
2025-04-25 21:21:01 -0300

    Acabou

--- app/src/main/java/com/example/lembretethiago/FirstFragment.kt
+++ app/src/main/java/com/example/lembretethiago/FirstFragment.kt
@@ -1,12 +1,18 @@
 package com.example.lembretethiago
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.text.set
 import androidx.navigation.fragment.findNavController
 import com.example.lembretethiago.databinding.FragmentFirstBinding
+import com.google.android.material.textfield.TextInputEditText
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,9 +20,6 @@ import com.example.lembretethiago.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,6 +34,28 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        lateinit var sharedPreferences: SharedPreferences
+
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        val editar = sharedPreferences.edit()
+
+        val salvar =  sharedPreferences.getString("TEXTO_SALVO", "")
+
+        binding.TextoDigitado.setText(salvar)
+
+        binding.buttonDeletar.setOnClickListener() {
+            binding.TextoDigitado.text?.clear()
+            editar.clear().commit()
+        }
+
+        binding.buttonSalvar.setOnClickListener(){
+            val texto = binding.TextoDigitado.text.toString()
+            Toast.makeText(context, "Texto salvo com sucesso!", Toast.LENGTH_SHORT).show()
+            editar.putString("TEXTO_SALVO",texto).apply()
+        }
+
 
     }
 
