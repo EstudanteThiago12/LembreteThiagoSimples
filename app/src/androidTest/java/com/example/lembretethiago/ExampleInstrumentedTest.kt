035b364e44497ec6b624675de384d8c08bd23d20
carlosbrenoe <4500445810@estudante.sed.sc.gov.br>
2025-06-27 20:48:06 -0300

    teste pronto

--- app/src/androidTest/java/com/example/lembretethiago/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lembretethiago/ExampleInstrumentedTest.kt
@@ -3,7 +3,9 @@ package com.example.lembretethiago
 import android.app.Activity
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -48,6 +50,12 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.nome_edt)).check(matches(isDisplayed()))
         onView(withId(R.id.sobrenome_edt)).check(matches(isDisplayed()))
 
+        onView(withId(R.id.button_second)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_second)).check(matches(withText("Proxima")))
+
+        onView(withId(R.id.buttonCadastro)).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonCadastro)).check(matches(withText("Cadastro")))
+        onView(withId(R.id.buttonCadastro)).perform(click())
         //---------------------------------------------------------------
 
         //Tela de Cadastro
@@ -67,8 +75,37 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.tvEmailCad)).check(matches(isDisplayed()))
         onView(withId(R.id.tvSenhaCad)).check(matches(isDisplayed()))
 
+        onView(withId(R.id.tvNomeCad)).perform(typeText("Thiago"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.tvNomeCad)).check(matches(withText("Thiago")))
+
+        onView(withId(R.id.tvEmailCad)).perform(typeText("Thiago@gmail.com"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.tvEmailCad)).check(matches(withText("Thiago@gmail.com")))
+
+        onView(withId(R.id.tvSenhaCad)).perform(typeText("123456789"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.tvSenhaCad)).check(matches(withText("123456789")))
+
+        onView(withId(R.id.btnCadastrarCad)).check(matches(isDisplayed()))
+        onView(withId(R.id.btnCadastrarCad)).check(matches(withText("Cadastrar")))
+        onView(withId(R.id.btnCadastrarCad)).perform(click())
+
         //----------------------------------------------------------
 
+        Thread.sleep(2000)
+
+        onView(withId(R.id.nome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.nome_edt)).perform(typeText("Thiago@gmail.com"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.nome_edt)).check(matches(withText("Thiago@gmail.com")))
+
+        onView(withId(R.id.sobrenome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.sobrenome_edt)).perform(typeText("123456789"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.sobrenome_edt)).check(matches(withText("123456789")))
+
+        onView(withId(R.id.button_second)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_second)).check(matches(withText("Proxima")))
+        onView(withId(R.id.button_second)).perform(click())
+
+
+        Thread.sleep(2000)
 
         //Tela de Lembrete
         onView(withId(R.id.Nome_txv)).check(matches(isDisplayed()))
@@ -76,75 +113,73 @@ class ExampleInstrumentedTest {
 
         onView(withId(R.id.TextoDigitado)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.TextoDigitado)).perform(typeText("lembrança"))
+        onView(withId(R.id.TextoDigitado)).perform(typeText("The Isle e um jogo"))
 
         onView(withId(R.id.buttonSalvar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonSalvar)).perform(click())
         onView(withText("Salvar")).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonSalvar)).perform(click())
 
-        onView(withId(R.id.TextoDigitado)).check(matches(withText("lembrança")))
-
-        onView(withId(R.id.buttonDeletar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonDeletar)).perform(click())
-        onView(withText("Deletar")).check(matches(isDisplayed()))
-
-        onView(withId(R.id.TextoDigitado)).perform(typeText(""))
+        onView(withId(R.id.TextoDigitado)).check(matches(withText("The Isle e um jogo")))
 
         onView(withId(R.id.buttonVoltar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonVoltar)).perform(click())
         onView(withText("Voltar")).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonVoltar)).perform(click())
 
+        Thread.sleep(2000)
 
-        //Testes
-
-        onView(withId(R.id.buttonCadastro)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonCadastro)).perform(click())
-        onView(withText("Cadastro")).check(matches(isDisplayed()))
-
-        onView(withId(R.id.tvNomeCad)).perform(typeText("Thiago"))
-        onView(withId(R.id.tvNomeCad)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvNomeCad)).check(matches(withText("Thiago")))
-
-        onView(withId(R.id.tvEmailCad)).perform(typeText("Thiago@gmail.com"))
-        onView(withId(R.id.tvEmailCad)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvEmailCad)).check(matches(withText("Thiago@gmail.com")))
-
-        onView(withId(R.id.tvSenhaCad)).perform(typeText("123456789"))
-        onView(withId(R.id.tvSenhaCad)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvSenhaCad)).check(matches(withText("123456789")))
-
-        onView(withId(R.id.btnCadastrarCad)).perform(click())
-        onView(withText("Cadastrar")).check(matches(isDisplayed()))
-
-        onView(withId(R.id.nome_edt)).perform(typeText("Thiago@gmail.com"))
         onView(withId(R.id.nome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.nome_edt)).perform(typeText("Thiago@gmail.com"), ViewActions.closeSoftKeyboard())
         onView(withId(R.id.nome_edt)).check(matches(withText("Thiago@gmail.com")))
 
-        onView(withId(R.id.sobrenome_edt)).perform(typeText("123456789"))
         onView(withId(R.id.sobrenome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.sobrenome_edt)).perform(typeText("123456789"), ViewActions.closeSoftKeyboard())
         onView(withId(R.id.sobrenome_edt)).check(matches(withText("123456789")))
 
         onView(withId(R.id.button_second)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_second)).check(matches(withText("Proxima")))
         onView(withId(R.id.button_second)).perform(click())
-        onView(withText("Proxima")).check(matches(isDisplayed()))
 
-        onView(withId(R.id.TextoDigitado)).perform(typeText("lembrança"))
+        Thread.sleep(2000)
 
-        onView(withId(R.id.buttonSalvar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonSalvar)).perform(click())
-        onView(withText("Salvar")).check(matches(isDisplayed()))
+        onView(withId(R.id.Nome_txv)).check(matches(isDisplayed()))
+        onView(withId(R.id.Nome_txv)).check(matches(withText("Olá, null")))
 
-        onView(withId(R.id.TextoDigitado)).check(matches(withText("lembrança")))
+        onView(withId(R.id.TextoDigitado)).check(matches(withText("The Isle e um jogo")))
 
         onView(withId(R.id.buttonDeletar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonDeletar)).perform(click())
         onView(withText("Deletar")).check(matches(isDisplayed()))
-
-        onView(withId(R.id.TextoDigitado)).perform(typeText(""))
+        onView(withId(R.id.buttonDeletar)).perform(click())
 
         onView(withId(R.id.buttonVoltar)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonVoltar)).perform(click())
         onView(withText("Voltar")).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonVoltar)).perform(click())
+        //Testes
+
+        Thread.sleep(2000)
+
+        onView(withId(R.id.nome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.nome_edt)).perform(typeText("Thiago@gmail.com"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.nome_edt)).check(matches(withText("Thiago@gmail.com")))
+
+        onView(withId(R.id.sobrenome_edt)).check(matches(isDisplayed()))
+        onView(withId(R.id.sobrenome_edt)).perform(typeText("123456789"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.sobrenome_edt)).check(matches(withText("123456789")))
+
+        onView(withId(R.id.button_second)).check(matches(isDisplayed()))
+        onView(withId(R.id.button_second)).check(matches(withText("Proxima")))
+        onView(withId(R.id.button_second)).perform(click())
+
+        Thread.sleep(2000)
+
+        onView(withId(R.id.Nome_txv)).check(matches(isDisplayed()))
+        onView(withId(R.id.Nome_txv)).check(matches(withText("Olá, null")))
+
+        onView(withId(R.id.TextoDigitado)).check(matches(withText("")))
+
+        onView(withId(R.id.buttonVoltar)).check(matches(isDisplayed()))
+        onView(withText("Voltar")).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonVoltar)).perform(click())
+
 
     }
 }
